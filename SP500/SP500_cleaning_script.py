abef1878bdb91538cf7944bb137018440e49dc62
mikeheheda <51770667+mikeheheda@users.noreply.github.com>
2022-05-19 23:25:57 -0700

    Data normalization and up/down indication

--- SP500/SP500_cleaning_script.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-#import the data and read as df
-df = pandas.read_csv("SP500.csv")   
-
-df = df.drop(['Adj Close'], axis = 1)
-
-#drop unnecessary observations since we only look at 2018's data
-df = df[4528:4779].reset_index()  
